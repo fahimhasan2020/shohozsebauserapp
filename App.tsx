160e5a6c8e765660b6e1cdb95a26c917a0abdf9f
FAHIM HASAN <31102498+fahimhasan2020@users.noreply.github.com>
2023-04-26 07:32:56 +0600

    updates

--- App.tsx
+++ App.tsx
@@ -1,38 +1,12 @@
+import 'react-native-gesture-handler';
 import React, { Component } from 'react'
-import { Text, View,StatusBar,Image } from 'react-native'
-import RNBootSplash from "react-native-bootsplash";
-import MapView,{Marker} from 'react-native-maps';
-import { sizes } from './src/constants/sizes';
-import { mapdesign, randomCoordinates } from './src/ui/mapdesign';
-
+import { Text, View } from 'react-native'
 
 export default class App extends Component {
-  componentDidMount(): void {
-    RNBootSplash.hide(); 
-  }
   render() {
     return (
-      <View style={{flex:1,backgroundColor:'green'}}>
-        <StatusBar  translucent={true} backgroundColor={'transparent'}/>
-        <MapView
-          style={{height:sizes.windowHeight+40,width:sizes.windowWidth}}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-          customMapStyle={mapdesign}
->
-{randomCoordinates.map((marker, index) => (<Marker
-  coordinate={marker}
-  key={index}
-  title={"Sujon"}
-  >
-       <Image source={require('./src/assets/donor.png')} style={{height:30,width:30}} />
-    </Marker>))}
-
-</MapView>
+      <View>
+        <Text> textInComponent </Text>
       </View>
     )
   }
